f3783a8a11027b838612eaa62d427d60bf055150
abdelilah <abdelilah@gmail.com>
2021-03-25 08:14:13 +0100

    add the file

---
+++ basic_calculator.py
@@ -0,0 +1,18 @@
+first_num = float(input("Enter the first number : "))
+op = input("Enter the operator : ")
+second_num = float(input("Enter the second number : "))
+
+if op == '+' :
+    print( first_num + second_num )
+elif op == '-' :
+    print( first_num - second_num )
+elif op == '*' :
+    print( first_num * second_num )
+elif op == '/' :
+    print( first_num / second_num )
+elif op == '//' :
+    print( first_num // second_num )
+elif op == '%' :
+    print( first_num % second_num )
+else :
+    print("you didn't enter a correct operator ! ")
