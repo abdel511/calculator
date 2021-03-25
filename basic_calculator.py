a2bbfffe6949bf689d11212db9e6e85f1a97ef2c
abdelilah <abdelilah@gmail.com>
2021-03-25 08:46:54 +0100

    add the power operation

--- basic_calculator.py
+++ basic_calculator.py
@@ -2,6 +2,8 @@ first_num = float(input("Enter the first number : "))
 op = input("Enter the operator : ")
 second_num = float(input("Enter the second number : "))
 
+
+
 if op == '+' :
     print( first_num + second_num )
 elif op == '-' :
@@ -14,5 +16,7 @@ elif op == '//' :
     print( first_num // second_num )
 elif op == '%' :
     print( first_num % second_num )
+elif op == '^' :
+    print( first_num ** second_num )
 else :
     print("you didn't enter a correct operator ! ")
