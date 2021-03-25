5fb7ff142c88f20a91b3119c90e6287eabff2739
abdelilah <abdelilah@gmail.com>
2021-03-25 09:42:14 +0100

    add the factorial operation

--- basic_calculator.py
+++ basic_calculator.py
@@ -2,6 +2,16 @@ first_num = float(input("Enter the first number : "))
 op = input("Enter the operator : ")
 second_num = float(input("Enter the second number : "))
 
+def factorial(num):
+    fact = 1
+    if num < 0:
+        print("can't calculate the factorial of a negative number!")
+    elif num == 0 :
+        print(1)
+    else:
+        for i in range(1,int(num)+1):
+            fact *= i
+        print(fact)
 
 
 if op == '+' :
@@ -18,5 +28,7 @@ elif op == '%' :
     print( first_num % second_num )
 elif op == '^' :
     print( first_num ** second_num )
+elif op == '!' :
+    print( factorial(first_num) )
 else :
     print("you didn't enter a correct operator ! ")
